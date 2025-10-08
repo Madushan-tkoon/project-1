cacb33a7ae2a702633329cac9c68bb2f15f67c1a
Madushan-tkoon <mtkoon@gmail.com>
2025-10-08 13:56:57 +0530

    update cart

--- src/app/t-shirt/[id]/page.jsx
+++ src/app/t-shirt/[id]/page.jsx
@@ -34,32 +34,32 @@ const ProductDetailsPage = () => {
   return (
     <div className='h-dvh bg-[#fff]' >
       <Header color="black"/>
-      <div className='mx-auto max-w-6xl flex flex-col md:flex-row pt-30 justify-between h-full items-center px-6 py-7'>
+      <div className='mx-auto max-w-6xl gap-6 flex flex-col md:flex-row pt-20 sm:pt-30 justify-between h-full items-center px-6 py-4 sm:py-7'>
         <div className='w-[350px] h-[350px] sm:w-[400px] sm:h-[400px] md:w-[450px] md:h-[450px] lg:w-[500px] lg:h-[500px] relative'>
           <DitailsTshirtModel logo={logo} material={material}/>
         </div>
 
-        <div className='flex flex-col gap-6 w-full md:w-[350px] lg:w-[430px]'>
+        <div className='flex flex-col gap-1 sm:gap-6 w-full md:w-[350px] lg:w-[430px]'>
             <div className='flex flex-col gap-1.5'>
-              <p className="text-sm uppercase font-semibold text-gray-500 tracking-wider">Super Soft</p>
-              <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight">{title}</h1>
-              <div className="flex items-center gap-2 mb-1">
+              <p className="text-[12px] sm:text-sm uppercase font-semibold text-gray-500 tracking-wider">Super Soft</p>
+              <h1 className="text-[23px] sm:text-4xl md:text-5xl font-bold leading-tight">{title}</h1>
+              <div className="flex items-center gap-2 mb-1 scale-85 sm:scale-100 ml-[-28px] sm:ml-0">
                 <div className="flex gap-1">
                   <StarIcon /><StarIcon /><StarIcon /><StarIcon /><StarIcon />
                 </div>
                 <span className="text-sm text-gray-500">323 Reviews</span>
               </div>
             </div>
-            <span className="text-[30px] font-bold text-gray-800">${price}</span>
+            <span className="text-[20px] sm:text-[30px]  font-bold text-gray-800">${price}</span>
             
             <div className='flex flex-col gap-2'>
-                <label>Size: {selectedSize} </label>
-                <div className='flex gap-3 text-[15px] mt-2'>
+                {/* <label>Size: {selectedSize} </label> */}
+                <div className='flex gap-3 text-[12px] sm:text-[15px] mt-2.5 '>
                   {availableSizes.map((size, index)=>(
                     <button 
                       key={index}
                       onClick={()=> setSelectedSize(size)}
-                      className={`w-[35px] h-[30px] flex pt-0.5 items-center justify-center rounded-[10px] cursor-pointer font-2 ${ selectedSize === size ? 'bg-black text-white' : 'bg-white text-black border border-gray-400' }`}
+                      className={`w-[27px] h-[24px] sm:w-[35px] sm:h-[30px]  flex items-center justify-center rounded-[4px] cursor-pointer font-2 ${ selectedSize === size ? 'bg-black text-white' : 'bg-white text-black border border-gray-400' }`}
                     >{size}</button>
                   ))}
                 </div>                
@@ -70,7 +70,7 @@ const ProductDetailsPage = () => {
                     const selectedProduct = {...product, size: selectedSize}
                     addToCart(selectedProduct)
                   }} 
-                  className='bg-black text-white w-full text-[14px] font-2 py-2.5 rounded-2xl cursor-pointer'>Add To Cart</button>
+                  className='bg-black text-white w-full text-[14px] font-2 py-0 sm:py-2.5 rounded-2xl cursor-pointer'>Add To Cart</button>
                 <button className='bg-black text-white w-[60px] text-[20px] font-3 py-2.5 rounded-2xl flex items-center justify-center cursor-pointer'><PiShareFatFill/></button>
             </div>
             
